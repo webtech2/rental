3a093180b322ba33b4f960a464e9f7857f72d231
Darja <darja.solodovnikova@lu.lv>
2020-05-03 16:13:54 +0300

    migrations added

---
+++ database/migrations/2020_05_03_125743_create_specifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specifications', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('make',50);     
+            $table->string('model',50);
+            $table->year('year');
+            $table->boolean('automatic');
+            $table->foreignId('class_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specifications');
+    }
+}
---
+++ database/migrations/2020_05_03_130433_create_cars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('reg_number',20);
+            $table->integer('mileage')->unsigned()->nullable();
+            $table->string('color',50);     
+            $table->text('condition')->nullable();
+            $table->foreignId('specification_id')->constrained();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
